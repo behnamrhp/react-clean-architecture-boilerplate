bcda31e69b2c276655a4d8210d332a44d4775e7a
behnamrhp <behnamrahimpour74@gmail.com>
2023-08-07 18:34:24 +0300

    fix store types

--- src/app/core/users/store/users-store.ts
+++ src/app/core/users/store/users-store.ts
@@ -3,10 +3,13 @@ import { combine, subscribeWithSelector } from "zustand/middleware";
 import toStream from "~/bootstrap/helper/store/store-to-stream";
 import { create } from "@mui/material/styles/createTransitions";
 import NUsersStore from "./i-users-store";
+import Users from "~/features/core/users/domain/entity/users";
 
 const usersStore = createStore(
   subscribeWithSelector(
-    combine({ users: [] }, (emit) => ({
+    combine<NUsersStore.UsersStore, NUsersStore.events>(
+      { users: [] as Users[] }, 
+      (emit) => ({
       addUsers(users) {
         emit(() => ({
           users,
