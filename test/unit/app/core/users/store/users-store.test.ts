8be547f2661fd5093897b69df53c6a939e307a4d
behnamrhp <behnamrahimpour74@gmail.com>
2023-08-10 12:54:12 +0300

    [FEAT]: add test for store

---
+++ test/unit/app/core/users/store/users-store.test.ts
@@ -0,0 +1,21 @@
+import usersStore from "~/app/core/users/store/users-store";
+import { MOCKED_USERS_LIST } from "test/common/factory";
+
+describe("Users-store:", () => {
+  beforeEach(() => {
+    usersStore.setState({ users: [] });
+  });
+  it("Should initilize correctly", () => {
+    expect(() => usersStore.getState()).toBeTruthy();
+  });
+
+  it("Should inital user correctly", () => {
+    usersStore.setState({ users: MOCKED_USERS_LIST });
+
+    expect(usersStore.getState().users).toStrictEqual(MOCKED_USERS_LIST);
+  });
+  it("Should add users correctly", () => {
+    usersStore.getState().addUsers(MOCKED_USERS_LIST);
+    expect(usersStore.getState().users).toStrictEqual(MOCKED_USERS_LIST);
+  });
+});
